48966e95f3b717d0be853c92c15e1f1f0b871219
Dante Arrighi <dante.arrighi@followoz.com>
2020-03-14 21:36:24 -0300

    initial commit

---
+++ DatingAppSPA/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ValueComponent } from './value/value.component';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'DatingAppSPA';
+}
